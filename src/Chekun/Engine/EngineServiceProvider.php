bd1784fbac81c1827fbc0b4f0ecab546ad561ea9
chekun <234267695@qq.com>
2014-02-24 14:48:39 +0800

    init project

---
+++ src/Chekun/Engine/EngineServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Chekun\Engine;
+
+use Illuminate\Support\ServiceProvider;
+
+class EngineServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('chekun/engine');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
